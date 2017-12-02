6efa98b4fb04db73063b81a4d81f22beef860779
Shinichii <denis.aurelienpl@gmail.com>
2017-12-02 18:00:30 +0100

    Add files via upload
    Ajout d'un point

--- install_part1.py
+++ install_part1.py
@@ -5,7 +5,7 @@ currentDirectory = os.path.dirname(os.path.abspath(__file__))
 version="3.7.0"
 yaraVersion="yara-"+version
 os.system("apt-get install automake libtool make gcc libssl-dev python-pip")
-urllib.urlretrieve("https://github.com/VirusTotal/yara/archive/v"+version+".tar.gz", yaraVersion + "tar.gz")
+urllib.urlretrieve("https://github.com/VirusTotal/yara/archive/v"+version+".tar.gz", yaraVersion + ".tar.gz")
 
 os.system("tar -zxf  "+ yaraVersion + ".tar.gz")
 os.system("pip install yara-python")
